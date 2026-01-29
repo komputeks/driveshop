439f9bc2e58c817377d21ae16b86d5b050f6be63
komputeks <xpatworld2021@gmail.com>
2026-01-29 04:46:52 +0300

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AuthModal from "@/components/AuthModal";
 import { Providers } from "./providers"; // ✅ ADD THIS
 
 const geistSans = Geist({
@@ -31,7 +30,6 @@ export default function RootLayout({
       >
         {/* ✅ Wrap app */}
         <Providers>
-          <AuthModal />
          {children}
         </Providers>
       </body>
