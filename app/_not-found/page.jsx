059bfb8f3fddd842cc8df8e567de5c2b326aad70
komputeks <xpatworld2021@gmail.com>
2026-01-31 17:21:19 +0300

    updates

---
+++ app/_not-found/page.jsx
@@ -0,0 +1,13 @@
+import Providers from "./providers";
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <Providers>
+      
+        <h1>Page Not Found</h1>
+      
+      </Providers>
+    </div>
+  );
+}
