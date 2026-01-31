483c55bc11601a76274d18af3a84723108530d5c
komputeks <xpatworld2021@gmail.com>
2026-01-31 17:39:53 +0300

    updates

--- app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">404 — Page Not Found</h1>
-    </div>
-  );
-}
