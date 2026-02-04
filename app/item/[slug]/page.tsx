9afa02dee0dc919830bd42ab83d580a422e5fdb1
komputeks <xpatworld2021@gmail.com>
2026-02-05 00:33:29 +0300

    updates

--- app/item/[slug]/page.tsx
+++ app/item/[slug]/page.tsx
@@ -2,7 +2,13 @@ import ItemDetails from "./ui/ItemDetails";
 import ItemActions from "./ui/ItemActions";
 import ItemComments from "./ui/ItemComments";
 
-export default async function ItemPage({ params }) {
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function ItemPage({ params }: PageProps) {
   return (
     <div className="max-w-4xl mx-auto space-y-8">
       <ItemDetails slug={params.slug} />
