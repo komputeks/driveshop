362ecc91c10942eb642b693c717a6c35d3814375
komputeks <xpatworld2021@gmail.com>
2026-02-09 13:32:00 +0300

    updates

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -5,4 +5,3 @@ import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return <SessionProvider>{children}</SessionProvider>;
- @providersAuthProvider}
