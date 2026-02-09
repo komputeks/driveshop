0cd1d4c54ca4c398094df7f45b4144e57849a790
komputeks <xpatworld2021@gmail.com>
2026-02-09 13:35:56 +0300

    updates

--- providers/AuthProvider.tsx
+++ providers/AuthProvider.tsx
@@ -1,7 +1,12 @@
-// @providers/AuthProvider.tsx
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import type { ReactNode } from "react";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function Providers({
+  children,
+}: {
+  children: ReactNode;
+}) {
   return <SessionProvider>{children}</SessionProvider>;
+}
