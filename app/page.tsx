fa4c279ada1736892a2b6df131ee952cd7309a2a
komputeks <xpatworld2021@gmail.com>
2026-02-11 15:07:57 +0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export const dynamic = "force-dynamic";
-// app/page.tsx
+
 import { ItemsGrid } from "@/components/itemsGrid";
 import { apiFetchServer } from "@/lib/typedFetch";
 import type { Item } from "@/lib/types";
@@ -36,48 +36,6 @@ export default async function HomePage() {
 
   return (
     <>
-      <pre>{JSON.stringify(items, null, 2)}</pre>
-      <ItemsGrid items={items} />
-    </>
-  );
-}
-import { ItemsGrid } from "@/components/itemsGrid";
-import { apiFetchServer } from "@/lib/typedFetch";
-import type { Item } from "@/lib/types";
-
-type ItemsListData = {
-  page: number;
-  limit: number;
-  total: number;
-  hasMore: boolean;
-  items: Item[];
-};
-
-export default async function HomePage() {
-  const url = process.env.NEXT_PUBLIC_API_BASE_URL!;
-
-  const res = await apiFetchServer<ItemsListData>(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    action: "items.list",
-    body: JSON.stringify({
-      action: "items.list",
-      page: 1,
-      limit: 40,
-    }),
-  });
-
-  if ("__overlay" in res) {
-    return null;
-  }
-
-  const items = res.data.items;
-
-  return (
-    <>
-      <pre>{JSON.stringify(items, null, 2)}</pre>
       <ItemsGrid items={items} />
     </>
   );
